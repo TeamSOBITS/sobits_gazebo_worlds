e5145586418f4c062848306560640b6a1cd09ef6
OnoFumiya <fumiyaono.choi@gmail.com>
2025-12-25 17:27:24 +0900

    Fix the path of gui custom config file

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ echo "╔══╣ Setup: SOBITS Gazebo Worlds (STARTING) ╠══╗"
 
 echo "export GZ_SIM_RESOURCE_PATH=${HOME}/colcon_ws/install/sobits_gazebo_worlds/share/sobits_gazebo_worlds/models" >> ~/.bashrc
 
-mkdir -p ~/.ignition/gazebo/6/
-sudo cp -r config/gui.config ${HOME}/.ignition/gazebo/6/
+mkdir -p ~/.gz/sim/8/
+sudo cp -r config/gui.config ${HOME}/.gz/sim/8/
 
-sudo apt-get update
+sudo apt update
 
 source ~/.bashrc
 
