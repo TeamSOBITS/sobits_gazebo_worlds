cb29f87b2d8a937d1baa8f1cd67d45dcfe5931d7
MrKeith99 <kvalentincardenas@hotmail.com>
2026-04-19 00:14:29 +0900

    [UPD] world as param

--- launch/world.launch.py
+++ launch/world.launch.py
@@ -1,19 +1,33 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value='rcjo2025_arena.world.xacro',
+        description=(
+            'World filename under sobits_gazebo_worlds/worlds. '
+            'Options: precomp2025_arena.world.xacro, '
+            'rcjo2025_arena.world.xacro, '
+            'rcjo2026_arena.world.xacro'
+        ),
+    )
+
     world_file_path = PathJoinSubstitution([
         get_package_share_directory('sobits_gazebo_worlds'),
         'worlds',
-        'rcjo2025_arena.world.xacro'
+        LaunchConfiguration('world')
     ])
 
     return LaunchDescription([
+        world_arg,
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
