415e2a7e735009b5d27c126b4ced0d8669ab3d3d
MrKeith99 <kvalentincardenas@hotmail.com>
2026-04-19 00:14:03 +0900

    [UPD] installer with required packages

--- install.sh
+++ install.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 echo "╔══╣ Setup: SOBITS Gazebo Worlds (STARTING) ╠══╗"
 
-echo "export GZ_SIM_RESOURCE_PATH=${HOME}/colcon_ws/install/sobits_gazebo_worlds/share/sobits_gazebo_worlds/models" >> ~/.bashrc
 
-mkdir -p ~/.gz/sim/8/
-sudo cp -r config/gui.config ${HOME}/.gz/sim/8/
+# Add Gazebo models to GZ_SIM_RESOURCE_PATH
+echo "export GZ_SIM_RESOURCE_PATH=\$\{GZ_SIM_RESOURCE_PATH\}:${HOME}/colcon_ws/install/sobits_gazebo_worlds/share/sobits_gazebo_worlds/models" >> ~/.bashrc
 
-sudo apt update
+# Install Gazebo Harmonic
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y \
+    gz-harmonic
 
-source ~/.bashrc
+# Clone required packages
+DIR=`pwd`
+cd ..
 
+# Clone required packages
+ros_packages=(
+    "gz_human_sim"
+    "tmc_wrs_gz"
+    "aws_small_house_world"
+)
+
+#Clone all packages
+for ((i = 0; i < ${#ros_packages[@]}; i++)) {
+    echo "Clonning: ${ros_packages[i]}"
+    git clone --recurse-submodules -b $ROS_DISTRO-devel https://github.com/TeamSOBITS/${ros_packages[i]}.git
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
+}
 
 echo "╚══╣ Setup: SOBITS Gazebo Worlds (FINISHED) ╠══╝"
